c01f194647f34210c9d9c8c03c9a821af385d69e
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-15 17:53:03 -0300

    [Import the Modal Componen] Install: npm i @material-ui/core  (material-ui.com) for the Modal Style  [Recipe.js] import material-ui  <Modal> open and clouse modal. Put null idREcipe, when clouse modal

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,37 +1,81 @@
-import React, {useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import { ModalContext } from '../context/ModalContext'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
 
+//material-ui (MOdal)
+function getModalStyle() {
+  const top = 50
+  const left = 50
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}))
+
+//***Component**
 const Recipe = ({ recipe }) => {
+  //configuration of modal "material-ui"
+  const [modalStyle] = useState(getModalStyle)
+  //open o clouse Modal
+  const [open, setOpen] = useState(false)
+
+  const classes = useStyles()
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   const { idDrink, strDrink, strDrinkThumb } = recipe
 
   //extrat value to context
-const { setIdRecipe } = useContext(ModalContext)
+  const { setIdRecipe } = useContext(ModalContext)
 
-//for to see recipe
-const handleOnClick = () =>{
+  //for to see recipe
+  const handleOnClick = () => {
     setIdRecipe(idDrink)
-}
-
-
+    handleOpen()
+  }
 
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header"> {strDrink}</h2>
-        <img
-          className="card-img-top"
-          src={strDrinkThumb} 
-          alt={strDrink}
-        />
-        
+        <img className="card-img-top" src={strDrinkThumb} alt={strDrink} />
+
         <div className="card-body">
-            <button 
+          <button
             type="button"
             className="btn btn-primary"
             onClick={handleOnClick}
-            >
-                See Recipe
-            </button>
+          >
+            See Recipe
+          </button>
+
+          <Modal open={open} onClose={()=> {
+              setIdRecipe(null)
+              handleClose()
+          }}>
+            <div style={modalStyle} className={classes.paper}>
+              <h1>from Modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
