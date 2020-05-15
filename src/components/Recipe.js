3b26039aa804de16e72229ce92719a5ec99ffcab
alejandroPolak <alejandro_polak@hotmail.com>
2020-05-15 16:03:08 -0300

    [Show info of recipe]

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,7 +7,20 @@ const Recipe = ({ recipe }) => {
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header"> {strDrink}</h2>
+        <img
+          className="card-img-top"
+          src={strDrinkThumb} 
+          alt={strDrink}
+        />
         
+        <div className="card-body">
+            <button 
+            type="button"
+            className="btn btn-primary"
+            >
+                See Recipe
+            </button>
+        </div>
       </div>
     </div>
   )
